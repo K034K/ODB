949b19baef2e7a8fd1e4d74b336a3e00150c2261
K034K <mrynskyiw@gmail.com>
2022-12-11 16:05:07 +0200

    add

---
+++ Laboratory4/TarkovShelter/src/test/kotlin/com/calculator/tarkovshelter/TarkovShelterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.calculator.tarkovshelter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TarkovShelterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
